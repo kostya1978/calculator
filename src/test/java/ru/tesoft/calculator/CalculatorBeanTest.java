7d09561167b695d763497db0c4f0c2448204d129
Konstantin Nikolaev <konstantin.v.nikolaev@gmail.com>
2016-02-10 05:12:29 +0300

    master - test commit

---
+++ src/test/java/ru/tesoft/calculator/CalculatorBeanTest.java
@@ -0,0 +1,14 @@
+package ru.tesoft.calculator;
+
+import org.junit.Test;
+/**
+ * Created by Konstantin on 08.02.2016.
+ */
+
+public class CalculatorBeanTest {
+
+    @Test
+    public void testAddition(){
+
+    }
+}
