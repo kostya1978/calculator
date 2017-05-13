2e44063643cace5a16919962141bfb085037ace6
Konstantin Nikolaev <konstantin.v.nikolaev@gmail.com>
2017-05-13 14:56:38 +0300

    Adding 3 integration tests created by Juliya Borisova

--- src/test/java/ru/tesoft/calculator/integration/CalculatorBeanIT.java
+++ src/test/java/ru/tesoft/calculator/integration/CalculatorBeanIT.java
@@ -7,12 +7,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.htmlunit.HtmlUnitDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.assertEquals;
 
@@ -31,7 +25,7 @@ public class CalculatorBeanIT {
     }
 
     @Test
-    public void test2Plus3(){
+    public void test2Plus3() {
         logger.debug("testAddition");
 
         // Open Calculator application in Chrome Browser:
@@ -71,4 +65,136 @@ public class CalculatorBeanIT {
             driver.quit();
         }
     }
+
+    @Test
+    public void test2Times3() {
+        logger.debug("testAddition");
+
+        // Open Calculator application in Chrome Browser:
+        driver.get("http://localhost:8080/Calculator");
+
+        // Find the "Button 2" element by it's id="btn2":
+        WebElement button2 = driver.findElement(By.id("btn2"));
+        // Click the "Button 2":
+        button2.click();
+
+        // Find the "Button X" element by it's id="btn*":
+        WebElement buttonX = driver.findElement(By.id("btn*"));
+        buttonX.click();
+
+        // Find the "Button 3" element by it's id="btn3":
+        WebElement button3 = driver.findElement(By.id("btn3"));
+        button3.click();
+
+        // Find the "Button =" element by it's id="btn=":
+        WebElement buttonEquals = driver.findElement(By.id("btn="));
+        buttonEquals.click();
+
+        // Find the Input Text element by it's id="display":
+        WebElement display = driver.findElement(By.id("display"));
+        String displayText = display.getAttribute("value");
+
+        try {
+            // Compare expected and actual results:
+            assertEquals("6", displayText);
+
+            // Let the user actually see something:
+            Thread.sleep(3500);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            //Close the browser
+            driver.quit();
+        }
+    }
+
+    @Test
+    public void test10Divide2() {
+        logger.debug("testAddition");
+
+        // Open Calculator application in Chrome Browser:
+        driver.get("http://localhost:8080/Calculator");
+
+        // Find the "Button 1" element by it's id="btn1":
+        WebElement button1 = driver.findElement(By.id("btn1"));
+        // Click the "Button 1":
+        button1.click();
+
+        // Find the "Button 0" element by it's id="btn0":
+        WebElement button0 = driver.findElement(By.id("btn0"));
+        button0.click();
+
+        // Find the "Button divide" element by it's id="btn/":
+        WebElement buttonDivide = driver.findElement(By.id("btn/"));
+        buttonDivide.click();
+
+        // Find the "Button 2" element by it's id="btn2":
+        WebElement button2 = driver.findElement(By.id("btn2"));
+        button2.click();
+
+        // Find the "Button =" element by it's id="btn=":
+        WebElement buttonEquals = driver.findElement(By.id("btn="));
+        buttonEquals.click();
+
+        // Find the Input Text element by it's id="display":
+        WebElement display = driver.findElement(By.id("display"));
+        String displayText = display.getAttribute("value");
+
+        try {
+            // Compare expected and actual results:
+            assertEquals("5", displayText);
+
+            // Let the user actually see something:
+            Thread.sleep(3500);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            //Close the browser
+            driver.quit();
+        }
+    }
+
+    @Test
+    public void test8Minus3() {
+        logger.debug("testAddition");
+
+        // Open Calculator application in Chrome Browser:
+        driver.get("http://localhost:8080/Calculator");
+
+        // Find the "Button 8" element by it's id="btn8":
+        WebElement button8 = driver.findElement(By.id("btn8"));
+        // Click the "Button 8":
+        button8.click();
+
+        // Find the "Button minus" element by it's id="btn-":
+        WebElement buttonMinus = driver.findElement(By.id("btn-"));
+        buttonMinus.click();
+
+        // Find the "Button 3" element by it's id="btn3":
+        WebElement button3 = driver.findElement(By.id("btn3"));
+        button3.click();
+
+        // Find the "Button =" element by it's id="btn=":
+        WebElement buttonEquals = driver.findElement(By.id("btn="));
+        buttonEquals.click();
+
+        // Find the Input Text element by it's id="display":
+        WebElement display = driver.findElement(By.id("display"));
+        String displayText = display.getAttribute("value");
+
+        try {
+            // Compare expected and actual results:
+            assertEquals("5", displayText);
+
+            // Let the user actually see something:
+            Thread.sleep(3500);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            //Close the browser
+            driver.quit();
+        }
+    }
+
+
 }
