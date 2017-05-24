6e4f7475f1f70c9f199af785bf05899253c94a2d
Konstantin Nikolaev <konstantin.v.nikolaev@gmail.com>
2017-05-24 18:17:39 +0300

    Fixing MailRu test

--- src/test/java/ru/tesoft/mailru/integration/MailRuIT.java
+++ src/test/java/ru/tesoft/mailru/integration/MailRuIT.java
@@ -1,12 +1,10 @@
 package ru.tesoft.mailru.integration;
 
 import org.apache.log4j.Logger;
+import org.junit.After;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import ru.tesoft.util.Util;
 
@@ -17,59 +15,54 @@ import static org.junit.Assert.assertEquals;
 
 public class MailRuIT {
 
-  static Logger logger = Logger.getLogger(MailRuIT.class);
-  private WebDriver driver;
+    static Logger logger = Logger.getLogger(MailRuIT.class);
+    private WebDriver driver;
 
-  @Before
-  public void setUp() throws Exception {
-    System.setProperty("webdriver.chrome.driver", Util.getCurrentProjectPath() + "/chromedriver.exe");
-    driver = new ChromeDriver();
-  }
-
-  @Test
-  public void test2Plus3() {
-    logger.debug("testAddition");
-
-    // Open mail.ru application in Chrome Browser:
-    driver.get("http://mail.ru");
-
-    // Find the "Button 2" element by it's id="btn2":
-    WebElement knopka2 = driver.findElement(By.id("btn2"));
-    // Click the "Button 2":
-    knopka2.click();
-
-    // Find the "Button +" element by it's id="btn+":
-    WebElement knopkaPlus = driver.findElement(By.id("btn+"));
-    knopkaPlus.click();
-
-    // Find the "Button 3" element by it's id="btn3":
-    WebElement knopka3 = driver.findElement(By.id("btn3"));
-    knopka3.click();
-
-    // Find the "Button =" element by it's id="btn=":
-    WebElement knopkaRavno = driver.findElement(By.id("btn="));
-    knopkaRavno.click();
-
-    // Find the Input Text element by it's id="display":
-    WebElement display = driver.findElement(By.id("display"));
-    String displayText = display.getAttribute("value");
-
-    try {
-      // Compare expected and actual results:
-      assertEquals("5", displayText);
-
-      // Let the user actually see something:
-      Thread.sleep(3500);
-    } catch (Exception e) {
-      e.printStackTrace();
-    } finally {
-      //Close the browser
-      driver.quit();
+    @Before
+    public void setUp() throws Exception {
+        System.setProperty("webdriver.chrome.driver", Util.getCurrentProjectPath() + "/chromedriver.exe");
+        driver = new ChromeDriver();
     }
-  }
 
+    @Test
+    public void testLogin() {
+        logger.debug("testLogin");
 
+        // Open mail.ru application in Chrome Browser:
+        driver.get("http://mail.ru");
 
+        // Find the "loginTextBox" element by it's id="mailbox__login":
+        WebElement loginTextBox = driver.findElement(By.id("mailbox__login"));
+        loginTextBox.sendKeys("selenium.webdriver");
+
+        // Find the "passwordTextBox" element by it's id="mailbox__password":
+        WebElement passwordTextBox = driver.findElement(By.id("mailbox__password"));
+        passwordTextBox.sendKeys("Gaspu3aQ");
+
+        // Find the "enterButton" element by it's id="mailbox__auth__button":
+        WebElement enterButton = driver.findElement(By.id("mailbox__auth__button"));
+
+        // Click the "enterButton":
+        enterButton.click();
+
+        // Find the "userEmail" element by it's id="PH_user-email":
+        // To avoid stale element reference error, we need to wait until element is loaded first:
+        Util.waitElementUntilPresent(By.id("PH_user-email"), driver);
+        WebElement userEmail = driver.findElement(By.id("PH_user-email"));
+        String displayText = userEmail.getText();
+
+        // Compare expected and actual results:
+        assertEquals("selenium.webdriver@mail.ru", displayText);
+
+        // Let the user actually see something:
+        Util.keepBrowserOpenFor3Seconds();
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        driver.quit();
+    }
 
 
 }
